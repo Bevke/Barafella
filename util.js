74bb18a31190bc9d21f8995cfe3e8de5ffcbd4be
Bevke <100231253+Bevke@users.noreply.github.com>
2024-10-25 22:17:39 +0300

    Add files via upload

---
+++ util.js
@@ -0,0 +1,6 @@
+var muaCopyrightElem = document.getElementById("mua-copyright");
+
+var muaCopyrightParagraph = document.createElement('p');
+muaCopyrightParagraph.innerHTML = 'Copyright &copy; '+new Date().getFullYear()+' Maifee Ul Asad';
+
+muaCopyrightElem.appendChild(muaCopyrightParagraph);
